3836c86c7532c496bea2e840a209d5732db3acdf
Arvind Tiwari <tiwariav@gmail.com>
2020-10-27 21:11:55 +0530

    fix: storybook css modules support

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,10 +1,45 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../../../**/*.stories.mdx",
-    "../../../**/*.stories.@(js|jsx|ts|tsx)"
+    "../../../**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    const cssRule = config.module.rules.find(
+      (rule) => rule.test.toString() === css_regex
+    );
+    config.module.rules = [
+      ...config.module.rules.filter(
+        (rule) => rule.test.toString() !== css_regex
+      ),
+      {
+        ...cssRule,
+        exclude: /\.module\.css$/,
+      },
+      {
+        ...cssRule,
+        test: /\.module\.css$/,
+        use: cssRule.use.map((rule) => {
+          if (rule && rule.loader && rule.loader.includes("css-loader")) {
+            return {
+              ...rule,
+              options: {
+                ...rule.options,
+                modules: true,
+              },
+            };
+          }
+          return rule;
+        }),
+      },
+    ];
+
+    // Return the altered config
+    return config;
+  },
+};
